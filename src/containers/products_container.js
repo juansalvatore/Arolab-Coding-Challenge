e9fa242ace054ffbc60e1b4515b6aa6be4103a93
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-21 11:47:06 -0300

    Create productsDisplay

--- src/containers/products_container.js
+++ src/containers/products_container.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -12,13 +13,35 @@ class Products extends Component {
     }
   }
 
+  displayProducts = () => {
+    return _.map(this.props.products, product => {
+      return (
+        <div key={product._id} id={product._id} className="productContainer">
+          <div className="imgContainer">
+            <img src={product.img.url} alt={product.name} />
+            <br />
+          </div>
+          <div className="textContainer">
+            <span className="category">{product.category}</span>
+            <br />
+            <span className="name">{product.name}</span>
+          </div>
+        </div>
+      )
+    })
+  }
+
   componentDidMount() {
     this.props.fetchProducts()
   }
 
   render() {
     console.log('Products:', this.props.products)
-    return <div>asdfsdf</div>
+    return (
+      <div className="productsCenter">
+        <div className="productsDisplayBox">{this.displayProducts()}</div>
+      </div>
+    )
   }
 }
 
--- src/reducers/reducer_products.js
+++ src/reducers/reducer_products.js
@@ -1,9 +1,10 @@
+import _ from 'lodash'
 import { FETCH_PRODUCTS } from '../actions/index'
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_PRODUCTS:
-      return action.payload.data
+      return _.mapKeys(action.payload.data, '_id')
   }
   return state
 }
